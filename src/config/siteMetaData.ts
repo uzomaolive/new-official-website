35e42e67b0bd3e4d798fb914f1befd9c04c68e4c
Olive Uzoma <olive.c.uzoma@gmail.com>
2025-03-04 11:54:12 +0100

    feat(metadata): update site metadata with refined description and author details
    - Updated  to better reflect expertise in software engineering and WordPress development.
    - Added  and  fields under  for better author attribution.
    - Ensured  aligns with the professional focus.

---
+++ src/config/siteMetaData.ts
@@ -0,0 +1,12 @@
+export const siteMetaData = {
+    title: "Olive Uzoma | Software Engineer & WordPress Developer",
+    description: "Software Engineer | WordPress Developer. passionate about using the tools available to me to efficiently solve problems",
+    siteUrl: "https://oliveuzoma.com",
+    locale: "en_US",
+    author: {
+      name: "Olive Uzoma",
+      linkedin: "@olive-uzoma",
+      github: "github.com/uzomaolive", 
+      email: "uzoma.olive.c@gmail.com",
+    },
+}
