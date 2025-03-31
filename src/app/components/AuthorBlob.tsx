34acca265d768f975698ab664c62c25b17039de9
Olive Uzoma <olive.c.uzoma@gmail.com>
2025-03-31 10:07:52 +0100

    - src/app/components/AuthorBlob.tsx:   - Updated the AuthorBlob component to use Next.js Image component for author image.

--- src/app/components/AuthorBlob.tsx
+++ src/app/components/AuthorBlob.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 type AuthorBlobProps = {
     image: string;
     date: string;
@@ -8,7 +10,13 @@ type AuthorBlobProps = {
 export default function AuthorBlob(props: AuthorBlobProps) {
     return (
       <div className="flex items-center mb-4">
-        <img src={props.image} className="w-12 rounded-full" />
+        <Image 
+          src={props.image} 
+          alt="Author"
+          width={50}
+          height={50}
+          className="rounded-full"
+        />
         <div className="ml-4 my-4">
           <p className="text-sm font-roboto text-bensonblack">{props.author}</p>
           <p className="text-xs font-roboto text-bensongrey">
