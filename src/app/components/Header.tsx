6cdd1327ed72fc0d62477ee58066d08b4d78c57e
Olive Uzoma <olive.c.uzoma@gmail.com>
2025-04-17 12:42:26 +0100

    Enhanced `Header.tsx` to consume theme context

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,12 @@
 "use client";
+
+import { useTheme } from "./ThemeProvider";
+import Link from "next/link";
 import Navbar from './Navbar';
 
 export default function Header() {
+  const { theme, setTheme } = useTheme();
+  
   return (
     <>
       <Navbar />
